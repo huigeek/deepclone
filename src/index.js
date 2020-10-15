edd22bfe41cc746f9f5934337c1e637b4c165a12
lixiaohui <463027965@qq.com>
2020-10-15 14:18:59 +0800

    add other type: function, regexp, date, set, map

--- src/index.js
+++ src/index.js
@@ -12,10 +12,48 @@ function deepclone (target) {
   // return types: null, number, string, boolean, undefined
   if (!isObject(target)) return target
 
-  // types left: Object, Array, Function, Date, RegExp, Map, Set, WeakMap, WeakSet...
+  // types: Function, Date, RegExp
+  if (target instanceof Function) {
+    const bodyReg = /(?<={)(.|\n)+(?=})/m
+    const paramReg = /(?<=\().+(?=\)\s+{)/
+    const funcString = target.toString()
+    if (target.prototype) {
+      const param = paramReg.exec(funcString)
+      const body = bodyReg.exec(funcString)
+      if (body) {
+        if (param) {
+          const paramArr = param[0].split(',')
+          return new Function(...paramArr, body[0])
+        } else {
+          return new Function(body[0])
+        }
+      } else {
+        return null
+      }
+    } else {
+      return eval(funcString)
+    }
+  }
+  else if (target instanceof RegExp) {
+    return new RegExp(target)
+  }
+  else if (target instanceof Date) {
+    return new Date(target)
+  }
+
   let result = getInit(target)
-  
-  for (const key in target) { // only Object, todo => other object
+
+  // types: Set, Map
+  if (target instanceof Set) {
+    target.forEach(v => result.add(v))
+    return result
+  }
+  else if (target instanceof Map) {
+    target.forEach((v, k) => result.set(k, v))
+    return result
+  }
+
+  for (const key in target) {
     result[key] = deepclone(target[key])
   }
 
--- test/index.js
+++ test/index.js
@@ -11,9 +11,69 @@ describe('my deepclone', () => {
     it('should clone string', () => assert.strictEqual('abc', deepclone('abc')))
     it('should clone boolean', () => assert.strictEqual(true, deepclone(true)))
     it('should clone undefined', () => assert.strictEqual(undefined, deepclone(undefined)))
+    it('should clone symbol', () => {
+      const a = Symbol('xx')
+      const b = deepclone(a)
+      assert.strictEqual(a, b)
+    })
   })
   describe('should clone object', () => {
-    it('should clone Object')
+    it('should clone basic object', () => {
+      const a = {
+        name: '一年级',
+        children: {
+          name: '3班',
+          numbers: 56
+        }
+      }
+      const b = deepclone(a)
+      assert.deepEqual(a, b)
+    })
+    it('should clone array', () => {
+      const a = [[3,2],[[1,5,[9,2]]],[0,5]]
+      const b = deepclone(a)
+      assert.deepEqual(a, b)
+    })
+    it('should clone function', () => {
+      const a = function xx (x, y) {
+        const m = x + 1
+        const n = y * 2
+        return m + n
+      }
+      const b = deepclone(a)
+      assert.notEqual(a, b)
+      assert.strictEqual(a(), b())
+    })
+    it('should clone regular expressions', () => {
+      const a = /[a-zA-Z]*/g
+      const b = deepclone(a)
+      assert.notEqual(a, b)
+      assert.strictEqual(a.source, b.source)
+      assert.strictEqual(a.flags, b.flags)
+    })
+    it('should clone date', () => {
+      const a = new Date()
+      const b = deepclone(a)
+      assert.notEqual(a, b)
+      assert.strictEqual(a.getTime(), b.getTime())
+    })
+    it('should clone set', () => {
+      const a = new Set([1, 3, 3, ,5])
+      const b = deepclone(a)
+      assert.notEqual(a, b)
+      assert.strictEqual(a.has(1), b.has(1))
+    })
+    it('should clone map', () => {
+      const a = new Map([
+        [1, 'one'],
+        [2, 'two'],
+        [3, 'three']
+      ])
+      const b = deepclone(a)
+      assert.notEqual(a, b)
+      assert.strictEqual(a.get(1), b.get(1))
+    })
+
   })
 })
 
